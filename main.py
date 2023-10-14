5e2057617f7989f4fa9bfd93f519cfee3d6727a3
Gilberto <gilberto.braga0021@gmail.com>
2023-10-14 19:29:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Calculadora simples
+from calcSimples import soma
+
+
+v1 = int(input("Informe o primeiro numero: "))
+v2 = int(input("Informe o segundo numero: "))
+
+total = soma(2, 2)
+
+print(f'\n{v1} + {v2} = {total}\n')
+
